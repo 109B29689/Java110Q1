1d1de103b8449e294c1ba2553199c5cdc87d34ea
109B29689 <91858061+109B29689@users.noreply.github.com>
2021-11-01 11:39:49 +0800

    Update Class01.java

--- 1101/Class01.java
+++ 1101/Class01.java
@@ -1,1 +1,24 @@
+class Cal_area{
+    int r;
+    int l;
+    int w;
+    int h;
+    void Spherevolume(int radius){
+        r=radius;
+        System.out.printf("%.1f\n",r*r*r*3.14159*4/3);
+    }
+    void Rectangularvolume(int length,int width,int height){
+        l=length;
+        w=width;
+        h=height;
+        System.out.println((l*w+w*h+l*h)*2); 
+    }
+}
+public class Class01 {
 
+    public static void main(String[] args) {
+        Cal_area ca=new Cal_area();
+        ca.Spherevolume(10);
+        ca.Rectangularvolume(4, 5, 6);
+    }
+}
